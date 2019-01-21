b90849c97f0a13343389027b022956b210cff200
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2019-01-21 12:19:46 -0500

    Add extra remote

--- cron/.cron/daily-morning.sh
+++ cron/.cron/daily-morning.sh
@@ -3,8 +3,3 @@
 source ~/.env
 echo "Every day in the morning on $(date)"
 set -x
-
-# BORG_LOG="/tmp/borg-lion-$(date -u +"%Y%m%d-%H%M%S")"
-# lion-backup &> $BORG_LOG
-# BORG_LOG="/tmp/borg-syl-$(date -u +"%Y%m%d-%H%M%S")"
-# syl-backup &> $BORG_LOG
--- cron/.cron/every-hour.sh
+++ cron/.cron/every-hour.sh
@@ -7,3 +7,4 @@ set -x
 
 backup-outdated syl
 backup-outdated lion
+backup-outdated nal
