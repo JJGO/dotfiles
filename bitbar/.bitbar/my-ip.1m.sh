a3610251a9c439bc57f614bc95588f86ba55a2ed
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2019-03-23 10:57:22 -0400

    Make IP bitbar tool emoji based. Add percentage tool

---
+++ bitbar/.bitbar/my-ip.1m.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+export LANG="en_US.UTF-8"
+export LC_COLLATE="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
+export LC_MESSAGES="en_US.UTF-8"
+export LC_MONETARY="en_US.UTF-8"
+export LC_NUMERIC="en_US.UTF-8"
+export LC_TIME="en_US.UTF-8"
+export LC_ALL=
+
+/Users/josejavier/bin/my-ip.py
