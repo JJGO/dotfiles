b4ffd1989a6d708c28a8cf5e670a2716abcbf197
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2019-12-07 19:20:49 -0500

    Add startup ipython scripts

---
+++ python/.ipython/profile_default/startup/01-jnt-plot.py
@@ -0,0 +1,7 @@
+# import jtplot submodule from jupyterthemes
+from jupyterthemes import jtplot
+
+# currently installed theme will be used to
+# set plot style if no arguments provided
+#jtplot.style()
+
