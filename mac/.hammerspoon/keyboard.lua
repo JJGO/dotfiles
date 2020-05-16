bb2c80167a9fff5393a65038d4bb655ba40507c2
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2020-05-16 12:17:09 -0400

    Hammerspoon keyboard fixes

--- mac/.hammerspoon/keyboard.lua
+++ mac/.hammerspoon/keyboard.lua
@@ -20,16 +20,22 @@ function openBrowser()
 end
 
 function openSublime()
-    -- d = os.date("%Y%m%d%H%M%S")
     os.execute('date -u +"~/Downloads/%Y%m%d%H%M%S.txt" | xargs /usr/local/bin/subl -n')
+end
+function openVimr()
+    d = os.date("%Y%m%d%H%M%S")
+    file = "/Users/josejavier/Downloads/" .. d .. ".md"
+    os.execute("/usr/local/bin/vimr " .. file)
 end
 
 -- ⌘ + ⏎ Opens New Terminal
 hs.hotkey.bind({"cmd"}, "return", openTerminal)
+
 -- ⌘ + ⇧ + ⏎ Opens New Browser Window
 hs.hotkey.bind({"cmd","shift"}, "return", openBrowser)
+
 -- Ctrl + ⇧ + ⏎
-hs.hotkey.bind({"ctrl", "cmd"}, "return", openSublime)
+hs.hotkey.bind({"ctrl", "cmd"}, "return", openVimr)
 
 -- Hyper+V types contents of clipboard
 hs.hotkey.bind(hyper, "V", function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) end)
@@ -40,5 +46,23 @@ hs.hotkey.bind(hyper, "`", function() hs.openConsole() end)
 -- Launch or Focus Activity Monitor
 hs.hotkey.bind(hyper, "M", function() hs.application.launchOrFocus("Activity Monitor") end)
 
+-- Launch or Focus Activity Monitor
+hs.hotkey.bind(hyper, "S", function() hs.application.launchOrFocus("Spotify") end)
+
 -- Provides a keyboard based window switcher (instead of app switcher)
-hs.hotkey.bind(hyper, "tab", function() hs.hints.windowHints() end)
+hs.hotkey.bind({"cmd", "alt"}, "tab", function() hs.hints.windowHints() end)
+
+-- Paste my email
+hs.hotkey.bind(hyper, "E", function() hs.eventtap.keyStrokes("josejavier.gonzalez.ortiz@gmail.com") end)
+
+-- Hyper+F makes toggles app zoom
+hs.hotkey.bind(hyper, "F", function() hs.window.focusedWindow():toggleZoom() end)
+
+-- Ctrl+Cmd + Escape -- Sleeps the Computer
+hs.hotkey.bind({"ctrl", "cmd"}, "escape", function() hs.caffeinate.systemSleep() end)
+
+-- Ctrl+Shift + Escape -- Sleeps the displays
+hs.hotkey.bind({"ctrl", "shift"}, "escape", function() os.execute("pmset displaysleepnow") end)
+
+-- Ctrl+Cmd+Alt + P -- Toggle Caps Lock
+hs.hotkey.bind({"ctrl", "cmd", "alt"}, "P", function() hs.hid.capslock.toggle() end)
