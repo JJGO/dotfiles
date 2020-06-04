efc1333a32935d6987a394437c660a06a28f44b0
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2020-06-04 11:24:45 -0400

    Major changes to Mac specific config

---
+++ mac/.hammerspoon/windows.lua
@@ -0,0 +1,13 @@
+hs.hotkey.bind({"cmd", "alt"}, "Left", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
