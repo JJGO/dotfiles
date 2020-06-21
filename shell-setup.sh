c9827c4c41cb031bf90b9d9b274a731dff938f85
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2020-06-21 14:31:41 -0400

    Fix on env setup

--- shell-setup.sh
+++ shell-setup.sh
@@ -33,7 +33,7 @@ fi
 
 if [[ ! -d $HOME/.tmux/plugins/tpm ]]; then
     mkdir -p $HOME/.tmux/plugins
-    gir clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+    git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
 fi
 
 
