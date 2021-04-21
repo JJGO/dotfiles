038d0c300fb9be198feab6b1f1c41be81001507a
Jose Javier <3844846+JJGO@users.noreply.github.com>
2021-04-21 12:33:44 -0400

    Make svg and pdf default format for matplotlib

--- python/.ipython/profile_default/startup/01-jnt-plot.py
+++ python/.ipython/profile_default/startup/01-jnt-plot.py
@@ -4,3 +4,6 @@ from jupyterthemes import jtplot
 # currently installed theme will be used to
 # set plot style if no arguments provided
 jtplot.style()
+
+from IPython.display import set_matplotlib_formats
+set_matplotlib_formats('svg', 'pdf')
